9d328dc35ef28c7eb211687b52a3959f55ff9e2d
mid0111 <m.tajima0111@gmail.com>
2014-08-02 17:26:16 +0900

    Change to using 'robot.hear' from 'request' module.

---
+++ runhubot.sh
@@ -0,0 +1,9 @@
+export PORT=9999
+
+export HUBOT_JENKINS_USER_NAME="user"
+export HUBOT_JENKINS_PASSWORD="password"
+export HUBOT_JENKINS_URL="http://127.0.0.1:8080/jenkins"
+export HUBOT_JENKINS_JOBS="test1, test2, test3"
+
+sh ./bin/hubot
+
